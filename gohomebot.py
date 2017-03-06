52f36d4f5cb5a98973214aeb651f72bf60f800ab
joomanba <joomanba@gmail.com>
2017-03-06 10:34:23 +0900

    My first Bot

---
+++ gohomebot.py
@@ -0,0 +1,22 @@
+import http.client
+import urllib.parse
+import json
+import ssl
+
+def lambda_handler():
+    params = urllib.parse.urlencode({'payload' : json.dumps({"text": "It's time to go home! Thanks for your hard work!"})})
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    context = ssl._create_unverified_context()
+#    conn = http.client.HTTPConnection('hooks.slack.com')
+    conn = http.client.HTTPSConnection('hooks.slack.com', context=context)
+    conn.request(
+        'POST',
+        '/services/T0KFH5M6U/B4D66M7UM/bHt9AtpYbxiw9gqAeQSVd9Rp',
+        params,
+        headers)
+
+    response = conn.getresponse()
+    print(response.status, response.reason, response.msg)
+    conn.close()
+
+lambda_handler()
